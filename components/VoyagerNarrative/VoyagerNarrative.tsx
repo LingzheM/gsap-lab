d497d4703c91fd104d8d9f90526f988bbee0bde9
mou <mou.reitetsu@gmail.com>
2026-06-05 22:27:30 +0900

    define root

--- components/VoyagerNarrative/VoyagerNarrative.tsx
+++ components/VoyagerNarrative/VoyagerNarrative.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { useRef } from "react"
+import styles from './VoyagerNarrative.module.css';
+
 export function VoyagerNarrative() {
+  
+  const rootRef = useRef<HTMLDivElement>(null);
+  const starsRef = useRef<HTMLDivElement>(null);
+  
   return (
-    <div>
-      
+    <div ref={rootRef} className={styles.root}>
+        <div ref={starsRef} className={styles.stars} aria-hidden />
     </div>
   )
 }
