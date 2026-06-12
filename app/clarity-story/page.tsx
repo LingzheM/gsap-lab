b6a2324d45f3017f25785992a3e51924f69fae18
mou <mou.reitetsu@gmail.com>
2026-06-13 00:38:16 +0900

    page add BackLink

--- app/clarity-story/page.tsx
+++ app/clarity-story/page.tsx
@@ -1,9 +1,11 @@
+import { BackLink } from "@/components/BackLink/BackLink";
 import { ClarityStory } from "@/components/ClarityStory/ClarityStory";
 
 
 export default function Page() {
   return (
     <>
+      <BackLink />;
       <ClarityStory />;
     </>
  
--- app/voyager/page.tsx
+++ app/voyager/page.tsx
@@ -1,8 +1,10 @@
-import { VoyagerNarrative } from "@/components/VoyagerNarrative/VoyagerNarrative";
+import { BackLink } from '@/components/BackLink/BackLink';
+import { VoyagerNarrative } from '@/components/VoyagerNarrative/VoyagerNarrative';
 
 export default function Page() {
   return (
     <>
+      <BackLink />
       <VoyagerNarrative />
     </>
   );
